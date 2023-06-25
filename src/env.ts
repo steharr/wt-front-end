21daf779d4cf7974582d2d16e9edaec8e29e6474
steharr <steharrold@gmail.com>
2023-06-25 03:15:44 +0100

    fix

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // url: 'https://steves-workout-tracker-api.onrender.com',
-  url: 'http://localhost:8080',
+  url: 'https://steves-workout-tracker-api.onrender.com',
+  // url: 'http://localhost:8080',
 };
